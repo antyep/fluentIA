f98a30e74abe4dbfd24cff78fcfb68d9f354fd4c
antyep <antyep6@gmail.com>
2024-08-06 10:13:43 +0200

    feat(whisper): Model defined to "base" and transcription defined as text.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import gradio as gr
+import whisper
 
-def translator():
-    pass
+def translator(audio_file):
+
+    model = whisper.load_model("base")
+    result = model.transcribe(audio_file)
+    transcription = result["text"]
+
+    transcription 
 
 web = gr.Interface(
     fn=translator,
