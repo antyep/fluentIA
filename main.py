8e127a58db08b80415f016f3a6d87dfc94f14a25
antyep <antyep6@gmail.com>
2024-08-06 14:09:37 +0200

    feat(textToSpeech): text format modified to text
    spanish swapped for english instead as main language
    
    Portuguese, Italian, French, Japanese, Spanish added

--- main.py
+++ main.py
@@ -7,23 +7,67 @@ from elevenlabs import VoiceSettings
 
 config = dotenv_values(".env")
 
-ELEVENLABS_API_KEY = config ("ELEVENLABS_API_KEY")
+ELEVENLABS_API_KEY = config["ELEVENLABS_API_KEY"]
 
 def translator(audio_file):
 
-    model = whisper.load_model("base")
-    result = model.transcribe(audio_file)
-    transcription = result["text"]
+    try:
+        model = whisper.load_model("base")
+        result = model.transcribe(audio_file, language="English", fp16=False)
 
-    en_transcription = Translator(to_lang="en").translate(transcription)
+        if result is None or "text" not in result:
+            raise ValueError("Error while transcribing text")
 
-    client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
+        transcription = result["text"]
+    except Exception as e:
+        raise gr.Error(f"There was an error while transcribing the text: {str(e)}")
 
-    response = client.text_to_speech.convert(
-            voice_id="2EiwWnXFnvU5JabPnv8n",
+    print(f"Original text: {transcription}")
+
+    try:
+        en_transcription = Translator(from_lang="en", to_lang="es").translate(transcription)
+        it_transcription = Translator(from_lang="en", to_lang="it").translate(transcription)
+        fr_transcription = Translator(from_lang="en", to_lang="fr").translate(transcription)
+        ja_transcription = Translator(from_lang="en", to_lang="ja").translate(transcription)
+        de_transcription = Translator(from_lang="en", to_lang="de").translate(transcription)
+        pt_transcription = Translator(from_lang="en", to_lang="pt").translate(transcription)
+
+
+    except Exception as e:
+        raise gr.Error(f"There was a mistake while creating the text: {str(e)}")
+
+    print(f"Text translated to Spanish: {en_transcription}")
+    print(f"Text translated to Italian: {it_transcription}")
+    print(f"Text translated to French: {fr_transcription}")
+    print(f"Text translated to Japanese: {ja_transcription}")
+    print(f"Text translated to German: {de_transcription}")
+    print(f"Text translated to Portuguese: {pt_transcription}")
+
+
+    try:
+        en_save_file_path = text_to_speech(en_transcription, "es")
+        it_save_file_path = text_to_speech(it_transcription, "it")
+        fr_save_file_path = text_to_speech(fr_transcription, "fr")
+        ja_save_file_path = text_to_speech(ja_transcription, "ja")
+        de_save_file_path = text_to_speech(de_transcription, "de")
+        pt_save_file_path = text_to_speech(pt_transcription, "pt")
+
+
+    except Exception as e:
+        raise gr.Error(f"There was a mistake during audio creation: {str(e)}")
+
+    return en_save_file_path, it_save_file_path, fr_save_file_path, ja_save_file_path, de_save_file_path, pt_save_file_path
+
+def text_to_speech(text: str, language: str) -> str:
+
+    try:
+        client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
+
+        response = client.text_to_speech.convert(
+            voice_id="2EiwWnXFnvU5JabPnv8n", 
             optimize_streaming_latency="0",
             output_format="mp3_22050_32",
-            text=en_transcription,
+            text=text,
             model_id="eleven_turbo_v2",
             voice_settings=VoiceSettings(
                 stability=0.0,
@@ -33,24 +77,36 @@ def translator(audio_file):
             ),
         )
 
-    save_file_path = "audio/en.mp3"
+        save_file_path = f"{language}.mp3"
 
-    with open(save_file_path, "w") as f:
-        for chunk in response:
-            if chunk:
-                f.write(chunk)
+        with open(save_file_path, "wb") as f:
+            for chunk in response:
+                if chunk:
+                    f.write(chunk)
+
+    except Exception as e:
+        raise gr.Error(f"There was an error while creating audio: {str(e)}")
+
     return save_file_path
 
 web = gr.Interface(
     fn=translator,
     inputs=gr.Audio(
         sources=["microphone"],
-        type="filepath"
+        type="filepath",
+        label="English"
     ),
-    outputs=[gr.Audio()],
+    outputs=[
+        gr.Audio(label="Spanish"),
+        gr.Audio(label="Italiano"),
+        gr.Audio(label="Française"),
+        gr.Audio(label="Japanese"),
+        gr.Audio(label="German"),
+        gr.Audio(label="Portuguese"),
+        
+    ],
     title="Voice Translator",
-    description="Voice Translator with IA"
+    description="Voice Translator from spoken English with IA"
 )
 
 web.launch()
-
