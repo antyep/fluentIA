88f340540c1065e892a19073806f4e7e340c46ef
antyep <antyep6@gmail.com>
2024-08-06 12:02:55 +0200

    feat(textToSpeech): Voice settings from elevenlabs defined

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import gradio as gr
 import whisper
 from translate import Translator
+from dotenv import dotenv_values
+from elevenlabs.client import ElevenLabs
+from elevenlabs import VoiceSettings
+
+config = dotenv_values(".env")
+
+ELEVENLABS_API_KEY = config ("ELEVENLABS_API_KEY")
 
 def translator(audio_file):
 
@@ -9,6 +16,24 @@ def translator(audio_file):
     transcription = result["text"]
 
     en_transcription = Translator(to_lang="en").translate(transcription)
+
+    client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
+
+    client.text_to_speech.convert(
+                response = client.text_to_speech.convert(
+            voice_id="pNInz6obpgDQGcFmaJgB",
+            optimize_streaming_latency="0",
+            output_format="mp3_22050_32",
+            text=en_transcription,
+            model_id="eleven_turbo_v2",
+            voice_settings=VoiceSettings(
+                stability=0.0,
+                similarity_boost=0.0,
+                style=0.0,
+                use_speaker_boost=True,
+            ),
+        )
+    )
 
 web = gr.Interface(
     fn=translator,
